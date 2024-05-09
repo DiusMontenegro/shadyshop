c1420ec13659f4fa569c0c51869838639112f3fc
Dre <montenegrodiussantos1@gmail.com>
2024-05-09 13:48:36 +0800

    ShadyShop - Auth user and protect routes

--- server/server.js
+++ server/server.js
@@ -6,12 +6,21 @@ import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cookieParser from 'cookie-parser';
 
 const port = process.env.PORT || 8000;
 
 connectDB(); // mongoDB connection
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
+
+// CORS middleware
 app.use(cors());
 
 app.get('/', (req, res) => {
