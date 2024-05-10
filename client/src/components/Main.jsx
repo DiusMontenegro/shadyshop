022b95ada46ab8c523113de6c4ec64f875060b62
Dre <montenegrodiussantos1@gmail.com>
2024-05-10 20:39:28 +0800

    ShadyShop - Setup create order and get order endpoints

--- client/src/components/Main.jsx
+++
@@ -1,5 +0,0 @@
-function Main({ children }) {
-    return <main className="container px-6 sm:px-8 py-4">{children}</main>;
-}
-
-export default Main;
--- client/src/components/Message.jsx
+++
@@ -1,5 +0,0 @@
-const Message = ({ children, className }) => {
-    return <div className={className}>{children}</div>;
-};
-
-export default Message;
