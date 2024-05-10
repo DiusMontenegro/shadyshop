e9efdda8302983d0fb2dcd13260fca8d528668d5
Dre <montenegrodiussantos1@gmail.com>
2024-05-10 15:29:02 +0800

    ShadyShop - Setup create order and get order endpoints

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -5,21 +5,63 @@ import Order from '../models/orderModel.js';
 // @route         POST /api/orders
 // @access        Private
 const addOrderItems = asyncHandler(async (req, res) => {
-    res.send('Order items added');
+    const {
+        orderItems,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+    } = req.body;
+
+    if (orderItems && orderItems.length === 0) {
+        res.status(400);
+        throw new Error('No Order Items');
+    } else {
+        const order = new Order({
+            orderItems: orderItems.map((x) => ({
+                ...x,
+                product: x._id,
+                _id: undefined,
+            })),
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice,
+        });
+
+        const createdOrder = await order.save();
+
+        res.status(201).json(createdOrder);
+    }
 });
 
 // @description   Get logged-in user orders
 // @route         GET /api/orders/mine
 // @access        Private
 const getMyOrders = asyncHandler(async (req, res) => {
-    res.send('Get my orders');
+    const orders = await Order.find({ user: req.user._id });
+    res.status(200).json(orders);
 });
 
 // @description   Get order by id
 // @route         GET /api/orders/:id
 // @access        Private
 const getOrderById = asyncHandler(async (req, res) => {
-    res.send('Get order by id');
+    const order = await Order.findById(req.params._id).populate(
+        'user',
+        'name email'
+    );
+
+    if (order) {
+        res.status(200).json(order);
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
 });
 
 // @description   Update order to paid
