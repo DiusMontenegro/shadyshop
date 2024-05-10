657c2c0395d9799038a572d248e736ffba4853db
Dre <montenegrodiussantos1@gmail.com>
2024-05-10 10:44:41 +0800

    ShadyShop - Design Shipping Page and protect routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import PageNotFound from './pages/PageNotFound';
 import Productpage from './pages/Productpage';
 import Loginpage from './pages/Loginpage';
 import Registerpage from './pages/Registerpage';
+import Shippingpage from './pages/Shippingpage';
+import ProtectedRoutes from './components/ProtectedRoutes';
 
 function App() {
     return (
@@ -15,10 +17,17 @@ function App() {
                 <Routes>
                     <Route index element={<Homepage />}></Route>
                     <Route path="product/:id" element={<Productpage />}></Route>
-                    <Route path="cart" element={<Cartpage />}></Route>
                     <Route path="register" element={<Registerpage />}></Route>
                     <Route path="login" element={<Loginpage />}></Route>
+                    <Route path="cart" element={<Cartpage />}></Route>
                     <Route path="*" element={<PageNotFound />}></Route>
+
+                    <Route path="" element={<ProtectedRoutes />}>
+                        <Route
+                            path="shipping"
+                            element={<Shippingpage />}
+                        ></Route>
+                    </Route>
                 </Routes>
             </BrowserRouter>
 
--- client/src/pages/Shippingpage.jsx
+++ client/src/pages/Shippingpage.jsx
@@ -1,7 +1,88 @@
-import React from 'react';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { saveShippingAddress } from '@/slices/cartSlice';
+
+import FormContainer from '@/components/FormContainer';
+import Navbar from '@/components/Navbar';
+import { Button, Input } from '@nextui-org/react';
 
 const Shippingpage = () => {
-    return <div>Shippingpage</div>;
+    const cart = useSelector((state) => state.cart);
+    const { shippingAddress } = cart;
+
+    const [address, setAddress] = useState(shippingAddress?.address || '');
+    const [city, setCity] = useState(shippingAddress?.city || '');
+    const [postalCode, setPostalCode] = useState(
+        shippingAddress?.postalCode || ''
+    );
+    const [country, setCountry] = useState(shippingAddress?.country || '');
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    function submitHandler(e) {
+        e.preventDefault();
+
+        dispatch(saveShippingAddress({ address, city, postalCode, country }));
+        navigate('/payment');
+    }
+
+    return (
+        <>
+            <Navbar />
+
+            <FormContainer>
+                <h1 className="text-2xl font-semibold mb-4">
+                    Shipping Address
+                </h1>
+                <form onSubmit={submitHandler} className="my-2 flex flex-col">
+                    <div className="flex flex-col w-full flex-wrap md:flex-nowrap gap-4">
+                        <Input
+                            type="text"
+                            label="Address"
+                            size="sm"
+                            placeholder="Enter your address"
+                            value={address}
+                            onChange={(e) => setAddress(e.target.value)}
+                        />
+                        <Input
+                            type="text"
+                            label="City"
+                            size="sm"
+                            placeholder="Enter your current city"
+                            value={city}
+                            onChange={(e) => setCity(e.target.value)}
+                        />
+                        <Input
+                            type="text"
+                            label="Postal Code"
+                            size="sm"
+                            placeholder="Postal Code"
+                            value={postalCode}
+                            onChange={(e) => setPostalCode(e.target.value)}
+                        />
+                        <Input
+                            type="text"
+                            label="Country"
+                            size="sm"
+                            placeholder="Country"
+                            value={country}
+                            onChange={(e) => setCountry(e.target.value)}
+                        />
+                        <Button
+                            className="text-md"
+                            variant="bordered"
+                            color="success"
+                            type="submit"
+                        >
+                            Continue
+                        </Button>
+                    </div>
+                </form>
+            </FormContainer>
+        </>
+    );
 };
 
 export default Shippingpage;
