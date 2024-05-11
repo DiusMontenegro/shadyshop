5fca473a0a0931a3b77d090db6e4699d7e3315ea
Dre <montenegrodiussantos1@gmail.com>
2024-05-11 18:59:58 +0800

    ShadyShop - Finish App

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -6,7 +6,9 @@ const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant='top' />
+        <div style={{ height: '200px', overflow: 'hidden' }}>
+          <Card.Img src={product.image} variant='top' className='w-100 h-100' />
+        </div>
       </Link>
 
       <Card.Body>
